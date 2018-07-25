d818be5e215c69b1b84d6dcc3c11310c2ff98a1a
vanessa_scappaticci@hotmail.co.uk <vanessa_scappaticci@hotmail.co.uk>
2018-07-25 18:41:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 /* Enter the code to remove the main node element under this comment */
+var element = document.createElement('div');
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+var ul = document.createElement('ul')
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+element.appendChild(ul);
+const newHeader = document.createElement('h1');
+newHeader.id="victory";
+document.body.appendChild(newHeader);
+newHeader.innerHTML= 'Sophoe is the Champion';
